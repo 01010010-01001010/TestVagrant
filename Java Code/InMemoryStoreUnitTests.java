91f5bc234ecf48da16ffe9482e9f3b6a2eed3d5f
Rajasekharb318 <rajasekhar.boddeda@aviso.com>
2023-05-05 00:02:35 +0530

    Java Code

---
+++ Java Code/InMemoryStoreUnitTests.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class RecentlyPlayedStoreTest {
+    @Test
+    public void testAddAndGetRecentSongs() {
+        RecentlyPlayedStore store = new RecentlyPlayedStore(3);
+        store.addSong("user1", "song1");
+        store.addSong("user1", "song2");
+        store.addSong("user1", "song3");
+        store.addSong("user1", "song4");
+        store.addSong("user1", "song2");
+        store.addSong("user1", "song1");
+        store.addSong("user2", "song1");
+        store.addSong("user2", "song2");
+        store.addSong("user2", "song3");
+
+        List<String> recentSongs1 = store.getRecentSongs("user1");
+        assertEquals(Arrays.asList("song4", "song2", "song1"), recentSongs1);
+
+        List<String> recentSongs2 = store.getRecentSongs("user2");
+        assertEquals(Arrays.asList("song1", "song2", "song3"), recentSongs2);
+
+        List<String> recentSongs3 = store.getRecentSongs("user3");
+        assertTrue(recentSongs3.isEmpty());
+    }
+}
